2d02d4d1a5f4cc4955d107a48a0c955e16a8a5ea
Josh Howe <josh@howe.io>
2019-06-16 22:03:34 -0400

    Build ESM & CJS with Rollup

--- rollup.config.js
+++ rollup.config.js
@@ -1,233 +1,201 @@
+import path from 'path';
+import alias from 'rollup-plugin-alias';
 import babel from 'rollup-plugin-babel';
 import commonjs from 'rollup-plugin-commonjs';
-import nodeResolve from 'rollup-plugin-node-resolve';
+import resolve from 'rollup-plugin-node-resolve';
 import replace from 'rollup-plugin-replace';
 import { terser } from 'rollup-plugin-terser';
 
+// Tell rollup how to resolve external imports
+const moduleAliases = { 'fdlib': path.resolve('node_modules/fdlib/dist/fdlib.es.js') }
+
+// Terser.js compress options
+const compress = {
+  pure_funcs: [
+    // fdlib
+    'domain_arr_max',
+    'domain_arrToStr',
+    'domain_str_decodeValue',
+    'domain_str_getValue',
+    'domain_bit_getValue',
+    'domain_sol_getValue',
+    'domain_num_createRange',
+    'domain_createEmpty',
+    'domain_createValue',
+    'domain_str_decodeValue',
+    'domain_toList',
+    'domain_max',
+    'domain_size',
+    'domain_min',
+    'domain_isSolved',
+    'domain_isZero',
+    'domain_hasNoZero',
+    'domain_hasZero',
+    'domain_isBool',
+    'domain_isBooly',
+    'domain_sharesNoElements',
+    'domain_createRange',
+    'domain_createRangeTrimmed',
+    'domain_toArr',
+    'domain_toStr',
+    'domain_toSmallest',
+    'domain_anyToSmallest',
+    'domain_arrToSmallest',
+    'domain_str_closeGaps',
+    'domain_containsValue',
+    'domain_num_containsValue',
+    'domain_createBoolyPair',
+    'domain__debug',
+    'domain_getFirstIntersectingValue',
+    'domain_getValue',
+    'domain_intersection',
+    'domain_intersectionValue',
+    'domain_isBoolyPair',
+    'domain_isEmpty',
+    'domain_numToStr',
+    'domain_removeGte',
+    'domain_removeGtUnsafe',
+    'domain_removeLte',
+    'domain_removeLtUnsafe',
+    'domain_removeValue',
+    'domain_resolveAsBooly',
+    'domain_str_encodeRange',
+    'domain_minus',
+    'domain_plus',
+    'INSPECT',
+    'getTerm',
+    'trie_create',
+    '_trie_debug',
+    'trie_get',
+    'trie_getNum',
+    'trie_getValueBitsize',
+    'trie_has',
+    'trie_hasNum',
+
+    // fdo
+    'config_clone',
+    'config_create',
+    'config_createVarStratConfig',
+    'constraint_create',
+    'distribution_getDefaults',
+    'distribution_markovSampleNextFromDomain',
+    'exporter_main',
+    'exporter_encodeVarName',
+    'markov_createLegend',
+    'markov_createProbVector',
+    'propagator_eqStepWouldReject',
+    'propagator_gtStepWouldReject',
+    'propagator_ltStepWouldReject',
+    'propagator_gteStepWouldReject',
+    'propagator_lteStepWouldReject',
+    'propagator_neqStepWouldReject',
+    'space_createRoot',
+    'space_getDomainArr',
+    'space_getVarSolveState',
+    'space_solution',
+    'space_toConfig',
+
+    // fdp
+    'bounty_getCounts',
+    'ml__debug',
+    'ml__opName',
+    'ml_countConstraints',
+    'ml_dec16',
+    'ml_dec32',
+    'ml_dec8',
+    'ml_getOpList',
+    'ml_getOpSizeSlow',
+    'ml_getRecycleOffset',
+    'ml_getRecycleOffsets',
+    'ml_hasConstraint',
+    'ml_recycleC3',
+    'ml_recycleVV',
+    'ml_sizeof',
+    'ml_validateSkeleton',
+  ],
+  pure_getters: true,
+  unsafe: true,
+  unsafe_comps: false, // TODO: find out why things break when this is true
+  warnings: false,
+};
+
 export default [
+  // Core Node builds
+  {
+    input: 'src/fdq.js',
+    plugins: [
+      alias(moduleAliases),
+      resolve(),
+      commonjs({ include: 'node_modules/**' }),
+      babel(),
+    ],
+    external: ['fs', 'path', 'events', 'module', 'util'],
+    treeshake: {
+      moduleSideEffects: false,
+      propertyReadSideEffects: false,
+    },
+    output: [
+      { file: 'dist/fdq.js', format: 'cjs', sourcemap: true },
+      { file: 'dist/fdq.es.js', format: 'esm' },
+    ],
+  },
+
   // UMD Development
   {
     input: 'src/fdq.js',
+    plugins: [
+      alias(moduleAliases),
+      resolve({ browser: true }),
+      commonjs({ include: 'node_modules/**' }),
+      babel(),
+      replace({ 'process.env.NODE_ENV': JSON.stringify('development') }),
+    ],
     output: {
-      file: 'dist/fdq.debug.js',
+      file: 'debug/fdq.browser.js',
       format: 'umd',
       name: 'FDQ',
-      indent: false,
-      sourceMap: true,
+      sourcemap: true,
     },
+  },
+
+  // UMD Production
+  {
+    input: 'src/fdq.js',
+    plugins: [
+      alias(moduleAliases),
+      resolve({ browser: true }),
+      commonjs({ include: 'node_modules/**' }),
+      babel(),
+      replace({ 'process.env.NODE_ENV': JSON.stringify('production') }),
+      terser({ compress, mangle: false }),
+    ],
     treeshake: {
       moduleSideEffects: false,
       propertyReadSideEffects: false
     },
-    plugins: [
-      nodeResolve(),
-      commonjs(),
-      babel(),
-      replace({ 'process.env.NODE_ENV': JSON.stringify('development') }),
+    output: [
+      { file: 'debug/fdq.browser.prod.js', format: 'umd', name: 'FDQ' },
     ],
   },
 
   // UMD Production
   {
     input: 'src/fdq.js',
-    output: {
-      file: 'dist/fdq.min.js',
-      format: 'umd',
-      name: 'FDQ',
-      indent: false,
-    },
+    plugins: [
+      alias(moduleAliases),
+      resolve({ browser: true }),
+      commonjs({ include: 'node_modules/**' }),
+      babel(),
+      replace({ 'process.env.NODE_ENV': JSON.stringify('production') }),
+      terser({ compress }),
+    ],
     treeshake: {
       moduleSideEffects: false,
       propertyReadSideEffects: false
     },
-    plugins: [
-      nodeResolve(),
-      commonjs(),
-      babel(),
-      replace({ 'process.env.NODE_ENV': JSON.stringify('production') }),
-      terser({
-        compress: {
-          pure_funcs: [
-            // fdlib
-
-            // assert
-            'ASSERT',
-            'ASSERT_ANYDOM',
-            'ASSERT_ARRDOM',
-            'ASSERT_BITDOM',
-            'ASSERT_LOG',
-            'ASSERT_NORDOM',
-            'ASSERT_NUMDOM',
-            'ASSERT_SET_LOG',
-            'ASSERT_SOLDOM',
-            'ASSERT_STRDOM',
-            'ASSERT_VARDOMS_SLOW',
-            'TRACE',
-            'TRACE_MORPH',
-            'TRACE_SILENT',
-            'isTracing',
-            'setTracing',
-
-            // domain_lib
-            'domain_arr_max',
-            'domain_arrToStr',
-            'domain_str_decodeValue',
-            'domain_str_getValue',
-            'domain_bit_getValue',
-            'domain_sol_getValue',
-            'domain_num_createRange',
-            'domain_createEmpty',
-            'domain_createValue',
-            'domain_str_decodeValue',
-            'domain_toList',
-            'domain_max',
-            'domain_size',
-            'domain_min',
-            'domain_isSolved',
-            'domain_isZero',
-            'domain_hasNoZero',
-            'domain_hasZero',
-            'domain_isBool',
-            'domain_isBooly',
-            'domain_sharesNoElements',
-            'domain_createRange',
-            'domain_createRangeTrimmed',
-            'domain_toArr',
-            'domain_toStr',
-            'domain_toSmallest',
-            'domain_anyToSmallest',
-            'domain_arrToSmallest',
-            'domain_str_closeGaps',
-            'domain_containsValue',
-            'domain_num_containsValue',
-            'domain_createBoolyPair',
-            'domain__debug',
-            'domain_getFirstIntersectingValue',
-            'domain_getValue',
-            'domain_intersection',
-            'domain_intersectionValue',
-            'domain_isBoolyPair',
-            'domain_isEmpty',
-            'domain_numToStr',
-            'domain_removeGte',
-            'domain_removeGtUnsafe',
-            'domain_removeLte',
-            'domain_removeLtUnsafe', // "Unsafe" here just means input is not validated first
-            'domain_removeValue',
-            'domain_resolveAsBooly',
-            'domain_str_encodeRange',
-            'domain_minus',
-            'domain_plus',
-
-            // helpers
-            'INSPECT',
-            'getTerm',
-
-            // trie
-            'trie_create',
-            '_trie_debug',
-            'trie_get',
-            'trie_getNum',
-            'trie_getValueBitsize',
-            'trie_has',
-            'trie_hasNum',
-
-            // fdo-specific
-
-            'domain_str_rangeIndexOf',
-            '_domain_str_mergeOverlappingRanges',
-            '_domain_str_quickSortRanges',
-            'distribution_varByList',
-            'distribution_varByMax',
-            'distribution_varByMarkov',
-            'distribution_varByMin',
-            'distribution_varByMinSize',
-            'distribution_varFallback',
-            'distribution_valueByList',
-            'distribution_valueByMarkov',
-            'distribution_valueByMax',
-            'distribution_valueByMid',
-            'distribution_valueByMin',
-            'distribution_valueByMinMaxCycle',
-            'distribution_valueBySplitMax',
-            'distribution_valueBySplitMin',
-            '_distribute_getNextDomainForVar',
-            // distribution/*
-            'distribution_varByList',
-            'distribution_varByMax',
-            'distribution_varByMarkov',
-            'distribution_varByMin',
-            'distribution_varByMinSize',
-            'distribution_varFallback',
-            'distribution_valueByList',
-            'distribution_valueByMarkov',
-            'distribution_valueByMax',
-            'distribution_valueByMid',
-            'distribution_valueByMin',
-            'distribution_valueByMinMaxCycle',
-            'distribution_valueBySplitMax',
-            'distribution_valueBySplitMin',
-            '_distribute_getNextDomainForVar',
-
-            // distribution/ testing-only
-            'domain_str_rangeIndexOf',
-            '_domain_str_mergeOverlappingRanges',
-            '_domain_str_quickSortRanges',
-
-            // fdp-specific
-
-            // bounty
-            'bounty__debug',
-            'bounty__debugMeta',
-            'bounty_getCounts',
-            'bounty_getMeta',
-            'bounty_getOffset',
-
-            // ml
-            'ml__debug',
-            'ml__opName',
-            //
-            'ml_countConstraints',
-            'ml_dec16',
-            'ml_dec32',
-            'ml_dec8',
-            //
-            'ml_getOpList',
-            'ml_getOpSizeSlow',
-            'ml_getRecycleOffset',
-            'ml_getRecycleOffsets',
-            'ml_hasConstraint',
-            'ml_heapSort16bitInline',
-            'ml_recycleC3',
-            'ml_recycleVV',
-            'ml_sizeof',
-            //
-            'ml_validateSkeleton',
-            '_ml_heapSort16bitInline',
-            'ml_any2c',
-            'ml_any2cr',
-            'ml_c2c2',
-            'ml_c2vv',
-            'ml_cr2c',
-            'ml_cr2c2',
-            'ml_cr2cr2',
-            'ml_cr2vv',
-            'ml_cr2vvv',
-            'ml_cx2cx',
-            'ml_vv2vv',
-            'ml_vvv2c2',
-            'ml_vvv2vv',
-            'ml_vvv2vvv',
-
-            // fdp
-            'solve',
-          ],
-          pure_getters: true,
-          unsafe: true,
-          unsafe_comps: false, // TODO: things break when this is true
-          warnings: false,
-        },
-      }),
+    output: [
+      { file: 'dist/fdq.browser.js', format: 'umd', name: 'FDQ' },
+      { file: 'dist/fdq.browser.es.js', format: 'esm' },
     ],
   },
 ];
--- src/fdq.js
+++ src/fdq.js
@@ -3,8 +3,47 @@ import FDP from 'fdp';
 
 import { setTerm } from 'fdlib';
 
-function solve(dsl, fdpOptions, fdoOptions) {
-  return FDP.solve(dsl, FDO.solve, fdpOptions, fdoOptions);
+// Everything silent by default
+setTerm({
+  log: _doNothing,
+  warn: _doNothing,
+  error: _doNothing,
+  trace: _doNothing,
+  time: _doNothing,
+  timeEnd: _doNothing,
+});
+
+const fdoDefaults = {
+  log: 0,
+  logger: _doNothing,
+  beforeSpace: _doNothing,
+  afterSpace: _doNothing,
+  _debug: false,
+  _debugConfig: false,
+  _debugSpace: false,
+  _debugSolver: false,
+};
+const fdpDefaults = {
+  log: 0,
+  debugDsl: false,
+  printDslBefore: _doNothing,
+  printDslAfter: _doNothing,
+  indexNames: false,
+  hashNames: false,
+  groupConstraints: false,
+};
+
+function _doNothing() {
+  return false;
+}
+
+function solve(dsl, fdpOptions = {}, fdoOptions = {}) {
+  return FDP.solve(
+    dsl,
+    FDO.solve,
+    { ...fdpDefaults, ...fdpOptions },
+    { ...fdoDefaults, ...fdoOptions }
+  );
 }
 
 export default { FDO, FDP, setTerm, solve };
